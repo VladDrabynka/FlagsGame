e556ba9719cdff4a1bc790a72af19937b7562459
Vlad <vlad.un1que.96@gmail.com>
2015-11-28 21:41:53 +0200

    Created autogenerated classes and first version of xml-database

---
+++ FlagGame/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlagGame
+{
+    public class Class1
+    {
+    }
+}
