1aed52ac5e7f7ebd0f21c3dc80beb0cd4e695431
Vlad Drabynka <vlad.un1que.96@gmail.com>
2015-12-06 01:03:32 +0200

    Changed some Form properties, add hints

--- FlagsGame/MainForm.Designer.cs
+++ FlagsGame/MainForm.Designer.cs
@@ -31,18 +31,24 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.pbFlag = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.btnVariantB = new System.Windows.Forms.Button();
-            this.btnVariantA = new System.Windows.Forms.Button();
-            this.btnVariantC = new System.Windows.Forms.Button();
             this.btnVariantD = new System.Windows.Forms.Button();
+            this.btnVariantC = new System.Windows.Forms.Button();
+            this.btnVariantA = new System.Windows.Forms.Button();
+            this.btnVariantB = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-            this.lbCountText = new System.Windows.Forms.Label();
             this.lbCount = new System.Windows.Forms.Label();
+            this.lbBest = new System.Windows.Forms.Label();
+            this.lbBestText = new System.Windows.Forms.Label();
+            this.lbCountText = new System.Windows.Forms.Label();
+            this.gbHints = new System.Windows.Forms.GroupBox();
+            this.btnSkip = new System.Windows.Forms.Button();
+            this.btnFifty = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbFlag)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
+            this.gbHints.SuspendLayout();
             this.SuspendLayout();
             // 
             // pbFlag
@@ -72,27 +78,16 @@
             this.tableLayoutPanel2.Size = new System.Drawing.Size(494, 141);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
-            // btnVariantB
+            // btnVariantD
             // 
-            this.btnVariantB.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnVariantB.Location = new System.Drawing.Point(250, 3);
-            this.btnVariantB.Name = "btnVariantB";
-            this.btnVariantB.Size = new System.Drawing.Size(241, 64);
-            this.btnVariantB.TabIndex = 1;
-            this.btnVariantB.Text = "button2";
-            this.btnVariantB.UseVisualStyleBackColor = true;
-            this.btnVariantB.Click += new System.EventHandler(this.buttonClick);
-            // 
-            // btnVariantA
-            // 
-            this.btnVariantA.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnVariantA.Location = new System.Drawing.Point(3, 3);
-            this.btnVariantA.Name = "btnVariantA";
-            this.btnVariantA.Size = new System.Drawing.Size(241, 64);
-            this.btnVariantA.TabIndex = 0;
-            this.btnVariantA.Text = "button1";
-            this.btnVariantA.UseVisualStyleBackColor = true;
-            this.btnVariantA.Click += new System.EventHandler(this.buttonClick);
+            this.btnVariantD.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnVariantD.Location = new System.Drawing.Point(250, 73);
+            this.btnVariantD.Name = "btnVariantD";
+            this.btnVariantD.Size = new System.Drawing.Size(241, 65);
+            this.btnVariantD.TabIndex = 3;
+            this.btnVariantD.Text = "button4";
+            this.btnVariantD.UseVisualStyleBackColor = true;
+            this.btnVariantD.Click += new System.EventHandler(this.buttonClick);
             // 
             // btnVariantC
             // 
@@ -105,16 +100,27 @@
             this.btnVariantC.UseVisualStyleBackColor = true;
             this.btnVariantC.Click += new System.EventHandler(this.buttonClick);
             // 
-            // btnVariantD
+            // btnVariantA
             // 
-            this.btnVariantD.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnVariantD.Location = new System.Drawing.Point(250, 73);
-            this.btnVariantD.Name = "btnVariantD";
-            this.btnVariantD.Size = new System.Drawing.Size(241, 65);
-            this.btnVariantD.TabIndex = 3;
-            this.btnVariantD.Text = "button4";
-            this.btnVariantD.UseVisualStyleBackColor = true;
-            this.btnVariantD.Click += new System.EventHandler(this.buttonClick);
+            this.btnVariantA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnVariantA.Location = new System.Drawing.Point(3, 3);
+            this.btnVariantA.Name = "btnVariantA";
+            this.btnVariantA.Size = new System.Drawing.Size(241, 64);
+            this.btnVariantA.TabIndex = 0;
+            this.btnVariantA.Text = "button1";
+            this.btnVariantA.UseVisualStyleBackColor = true;
+            this.btnVariantA.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // btnVariantB
+            // 
+            this.btnVariantB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnVariantB.Location = new System.Drawing.Point(250, 3);
+            this.btnVariantB.Name = "btnVariantB";
+            this.btnVariantB.Size = new System.Drawing.Size(241, 64);
+            this.btnVariantB.TabIndex = 1;
+            this.btnVariantB.Text = "button2";
+            this.btnVariantB.UseVisualStyleBackColor = true;
+            this.btnVariantB.Click += new System.EventHandler(this.buttonClick);
             // 
             // tableLayoutPanel1
             // 
@@ -125,6 +131,7 @@
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.pbFlag, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.gbHints, 2, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
@@ -134,23 +141,55 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(834, 487);
             this.tableLayoutPanel1.TabIndex = 1;
-            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // tableLayoutPanel3
             // 
             this.tableLayoutPanel3.ColumnCount = 2;
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.375F));
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.625F));
-            this.tableLayoutPanel3.Controls.Add(this.lbCountText, 0, 0);
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 79.375F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.625F));
             this.tableLayoutPanel3.Controls.Add(this.lbCount, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.lbBest, 1, 1);
+            this.tableLayoutPanel3.Controls.Add(this.lbBestText, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.lbCountText, 0, 0);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.MaximumSize = new System.Drawing.Size(160, 334);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 2;
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.47305F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 86.52695F));
             this.tableLayoutPanel3.Size = new System.Drawing.Size(160, 334);
             this.tableLayoutPanel3.TabIndex = 2;
+            // 
+            // lbCount
+            // 
+            this.lbCount.AutoSize = true;
+            this.lbCount.Location = new System.Drawing.Point(130, 15);
+            this.lbCount.Margin = new System.Windows.Forms.Padding(3, 15, 0, 0);
+            this.lbCount.Name = "lbCount";
+            this.lbCount.Size = new System.Drawing.Size(13, 13);
+            this.lbCount.TabIndex = 1;
+            this.lbCount.Text = "0";
+            // 
+            // lbBest
+            // 
+            this.lbBest.AutoSize = true;
+            this.lbBest.Location = new System.Drawing.Point(130, 59);
+            this.lbBest.Margin = new System.Windows.Forms.Padding(3, 15, 0, 0);
+            this.lbBest.Name = "lbBest";
+            this.lbBest.Size = new System.Drawing.Size(13, 13);
+            this.lbBest.TabIndex = 3;
+            this.lbBest.Text = "0";
+            // 
+            // lbBestText
+            // 
+            this.lbBestText.AutoSize = true;
+            this.lbBestText.Location = new System.Drawing.Point(15, 59);
+            this.lbBestText.Margin = new System.Windows.Forms.Padding(15, 15, 0, 0);
+            this.lbBestText.Name = "lbBestText";
+            this.lbBestText.Size = new System.Drawing.Size(63, 13);
+            this.lbBestText.TabIndex = 2;
+            this.lbBestText.Text = "Best streak:";
             // 
             // lbCountText
             // 
@@ -162,15 +201,40 @@
             this.lbCountText.TabIndex = 0;
             this.lbCountText.Text = "Right answers:";
             // 
-            // lbCount
+            // gbHints
             // 
-            this.lbCount.AutoSize = true;
-            this.lbCount.Location = new System.Drawing.Point(98, 15);
-            this.lbCount.Margin = new System.Windows.Forms.Padding(3, 15, 0, 0);
-            this.lbCount.Name = "lbCount";
-            this.lbCount.Size = new System.Drawing.Size(13, 13);
-            this.lbCount.TabIndex = 1;
-            this.lbCount.Text = "0";
+            this.gbHints.AutoSize = true;
+            this.gbHints.Controls.Add(this.btnSkip);
+            this.gbHints.Controls.Add(this.btnFifty);
+            this.gbHints.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbHints.Location = new System.Drawing.Point(669, 3);
+            this.gbHints.Name = "gbHints";
+            this.gbHints.Size = new System.Drawing.Size(162, 334);
+            this.gbHints.TabIndex = 3;
+            this.gbHints.TabStop = false;
+            this.gbHints.Text = "Hints";
+            // 
+            // btnSkip
+            // 
+            this.btnSkip.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSkip.Location = new System.Drawing.Point(3, 56);
+            this.btnSkip.Name = "btnSkip";
+            this.btnSkip.Size = new System.Drawing.Size(156, 40);
+            this.btnSkip.TabIndex = 1;
+            this.btnSkip.Text = "Skip";
+            this.btnSkip.UseVisualStyleBackColor = true;
+            this.btnSkip.Click += new System.EventHandler(this.btnSkip_Click);
+            // 
+            // btnFifty
+            // 
+            this.btnFifty.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnFifty.Location = new System.Drawing.Point(3, 16);
+            this.btnFifty.Name = "btnFifty";
+            this.btnFifty.Size = new System.Drawing.Size(156, 40);
+            this.btnFifty.TabIndex = 0;
+            this.btnFifty.Text = "50/50";
+            this.btnFifty.UseVisualStyleBackColor = true;
+            this.btnFifty.Click += new System.EventHandler(this.btnFifty_Click);
             // 
             // MainForm
             // 
@@ -178,6 +242,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(834, 487);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "Flags";
@@ -186,8 +251,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.pbFlag)).EndInit();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.tableLayoutPanel3.ResumeLayout(false);
             this.tableLayoutPanel3.PerformLayout();
+            this.gbHints.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -204,6 +271,11 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
         private System.Windows.Forms.Label lbCountText;
         private System.Windows.Forms.Label lbCount;
+        private System.Windows.Forms.GroupBox gbHints;
+        private System.Windows.Forms.Button btnFifty;
+        private System.Windows.Forms.Button btnSkip;
+        private System.Windows.Forms.Label lbBestText;
+        private System.Windows.Forms.Label lbBest;
 
     }
 }
--- FlagsGame/MainForm.cs
+++ FlagsGame/MainForm.cs
@@ -18,6 +18,7 @@ namespace FlagsGame
         FlagDatabase flagDb = new FlagDatabase();
         int count = 0;
         Button trueButton;
+        List<Button> arrayBut = new List<Button>();
 
         public MainForm()
         {
@@ -59,6 +60,39 @@ namespace FlagsGame
         private void MainForm_SizeChanged(object sender, EventArgs e)
         {
             Draw();
+            float currentSize;
+            if (WindowState == FormWindowState.Maximized)
+            {
+                currentSize = btnVariantA.Font.Size;
+                currentSize += 4.0F;
+                foreach (var btn in arrayBut)
+                    btn.Font = new Font(btn.Font.Name, currentSize, btn.Font.Style, btn.Font.Unit);
+
+                btnFifty.Font = new Font(btnFifty.Font.Name, currentSize, btnFifty.Font.Style, btnFifty.Font.Unit);
+                btnSkip.Font = new Font(btnFifty.Font.Name, currentSize, btnFifty.Font.Style, btnFifty.Font.Unit);
+
+                lbCount.Font = new Font(lbCount.Font.Name, currentSize, lbCount.Font.Style, lbCount.Font.Unit);
+                lbCountText.Font = new Font(lbCountText.Font.Name, currentSize, lbCountText.Font.Style, lbCountText.Font.Unit); 
+                lbBest.Font = new Font(lbCountText.Font.Name, currentSize, lbCountText.Font.Style, lbCountText.Font.Unit);
+                lbBestText.Font = new Font(lbCountText.Font.Name, currentSize, lbCountText.Font.Style, lbCountText.Font.Unit);
+                gbHints.Font = new Font(lbCountText.Font.Name, currentSize, lbCountText.Font.Style, lbCountText.Font.Unit);
+            }
+            if (WindowState == FormWindowState.Normal)
+            {
+                currentSize = btnVariantA.Font.Size;
+                currentSize -= 4.0F;
+                foreach (var btn in arrayBut)
+                    btn.Font = new Font(btn.Font.Name, currentSize, btn.Font.Style, btn.Font.Unit);
+
+                btnFifty.Font = new Font(btnFifty.Font.Name, currentSize, btnFifty.Font.Style, btnFifty.Font.Unit);
+                btnSkip.Font = new Font(btnFifty.Font.Name, currentSize, btnFifty.Font.Style, btnFifty.Font.Unit);
+
+                lbCount.Font = new Font(lbCount.Font.Name, currentSize, lbCount.Font.Style, lbCount.Font.Unit);
+                lbCountText.Font = new Font(lbCountText.Font.Name, currentSize, lbCountText.Font.Style, lbCountText.Font.Unit);
+                lbBest.Font = new Font(lbCountText.Font.Name, currentSize, lbCountText.Font.Style, lbCountText.Font.Unit);
+                lbBestText.Font = new Font(lbCountText.Font.Name, currentSize, lbCountText.Font.Style, lbCountText.Font.Unit);
+                gbHints.Font = new Font(lbCountText.Font.Name, currentSize, lbCountText.Font.Style, lbCountText.Font.Unit);
+            }
         }
 
         private void loadFlagForm()
@@ -66,28 +100,40 @@ namespace FlagsGame
             flag = flagDb.getRandomFlag();
             Draw();
             Random rnd = new Random();
-            List<Button> array = new List<Button>();
-            array.Add(btnVariantA);
-            array.Add(btnVariantB);
-            array.Add(btnVariantC);
-            array.Add(btnVariantD);
-            ShuffleList.Shuffle(array);
-            foreach (var btn in array)
+            arrayBut.Add(btnVariantA);
+            arrayBut.Add(btnVariantB);
+            arrayBut.Add(btnVariantC);
+            arrayBut.Add(btnVariantD);
+            foreach (var btn in arrayBut)
+                btn.Enabled = true;
+            ShuffleList.Shuffle(arrayBut);
+            foreach (var btn in arrayBut)
                 btn.Text = flagDb.getRandomFlag().Name;
-            trueButton = array[0];
-            array[0].Text = flag.Name;
+            trueButton = arrayBut[0];
+            arrayBut[0].Text = flag.Name;
             flagDb.clearWorkFlags();
         }
 
-        private void fillVariants(Flag flag)
+            
+        private void btnFifty_Click(object sender, EventArgs e)
         {
             Random rnd = new Random();
+            List<Button> btns = arrayBut;
+            btns.RemoveAt(0);
+            int rndValue;
+            for (int i = 0; i < 2; ++i)
+            {
+                rndValue = rnd.Next(0, btns.Count());
+                btns[rndValue].Enabled = false;
+                btns.RemoveAt(rndValue);
+            }
+            btnFifty.Enabled = false;
         }
 
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        private void btnSkip_Click(object sender, EventArgs e)
         {
-
+            loadFlagForm();
+            btnSkip.Enabled = false;
         }
-
     }
 }
