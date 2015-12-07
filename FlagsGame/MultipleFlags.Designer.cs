1e9acf1e2ad4fcd4c86e7ce0a0a1fb62ebf7dd91
Vlad Drabynka <vlad.un1que.96@gmail.com>
2015-12-07 17:32:52 +0200

    Added new branch, create part of new gamemode

---
+++ FlagsGame/MultipleFlags.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace FlagsGame
+{
+    partial class MultipleFlags
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MultipleFlags));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.pbFlag1 = new System.Windows.Forms.PictureBox();
+            this.pbFlag2 = new System.Windows.Forms.PictureBox();
+            this.pbFlag3 = new System.Windows.Forms.PictureBox();
+            this.pbFlag4 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnVariantA = new System.Windows.Forms.Button();
+            this.btnVariantB = new System.Windows.Forms.Button();
+            this.btnVariantC = new System.Windows.Forms.Button();
+            this.btnVariantD = new System.Windows.Forms.Button();
+            this.cbVariantA = new System.Windows.Forms.ComboBox();
+            this.cbVariantC = new System.Windows.Forms.ComboBox();
+            this.cbVariantB = new System.Windows.Forms.ComboBox();
+            this.cbVariantD = new System.Windows.Forms.ComboBox();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFlag1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFlag2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFlag3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFlag4)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnApply, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 65.0924F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34.9076F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(834, 487);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.pbFlag1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.pbFlag2, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.pbFlag3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.pbFlag4, 1, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(128, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(577, 310);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // pbFlag1
+            // 
+            this.pbFlag1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbFlag1.Location = new System.Drawing.Point(3, 3);
+            this.pbFlag1.Name = "pbFlag1";
+            this.pbFlag1.Size = new System.Drawing.Size(282, 149);
+            this.pbFlag1.TabIndex = 0;
+            this.pbFlag1.TabStop = false;
+            // 
+            // pbFlag2
+            // 
+            this.pbFlag2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbFlag2.Location = new System.Drawing.Point(291, 3);
+            this.pbFlag2.Name = "pbFlag2";
+            this.pbFlag2.Size = new System.Drawing.Size(283, 149);
+            this.pbFlag2.TabIndex = 1;
+            this.pbFlag2.TabStop = false;
+            // 
+            // pbFlag3
+            // 
+            this.pbFlag3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbFlag3.Location = new System.Drawing.Point(3, 158);
+            this.pbFlag3.Name = "pbFlag3";
+            this.pbFlag3.Size = new System.Drawing.Size(282, 149);
+            this.pbFlag3.TabIndex = 2;
+            this.pbFlag3.TabStop = false;
+            // 
+            // pbFlag4
+            // 
+            this.pbFlag4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbFlag4.Location = new System.Drawing.Point(291, 158);
+            this.pbFlag4.Name = "pbFlag4";
+            this.pbFlag4.Size = new System.Drawing.Size(283, 149);
+            this.pbFlag4.TabIndex = 3;
+            this.pbFlag4.TabStop = false;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 4;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel3.Controls.Add(this.btnVariantA, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnVariantB, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnVariantC, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.btnVariantD, 2, 1);
+            this.tableLayoutPanel3.Controls.Add(this.cbVariantA, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.cbVariantC, 1, 1);
+            this.tableLayoutPanel3.Controls.Add(this.cbVariantB, 3, 0);
+            this.tableLayoutPanel3.Controls.Add(this.cbVariantD, 3, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(128, 319);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(577, 165);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // btnVariantA
+            // 
+            this.btnVariantA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnVariantA.Location = new System.Drawing.Point(3, 3);
+            this.btnVariantA.Name = "btnVariantA";
+            this.btnVariantA.Size = new System.Drawing.Size(195, 76);
+            this.btnVariantA.TabIndex = 0;
+            this.btnVariantA.Text = "button1";
+            this.btnVariantA.UseVisualStyleBackColor = true;
+            // 
+            // btnVariantB
+            // 
+            this.btnVariantB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnVariantB.Location = new System.Drawing.Point(290, 3);
+            this.btnVariantB.Name = "btnVariantB";
+            this.btnVariantB.Size = new System.Drawing.Size(195, 76);
+            this.btnVariantB.TabIndex = 1;
+            this.btnVariantB.Text = "button2";
+            this.btnVariantB.UseVisualStyleBackColor = true;
+            // 
+            // btnVariantC
+            // 
+            this.btnVariantC.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnVariantC.Location = new System.Drawing.Point(3, 85);
+            this.btnVariantC.Name = "btnVariantC";
+            this.btnVariantC.Size = new System.Drawing.Size(195, 77);
+            this.btnVariantC.TabIndex = 2;
+            this.btnVariantC.Text = "button3";
+            this.btnVariantC.UseVisualStyleBackColor = true;
+            // 
+            // btnVariantD
+            // 
+            this.btnVariantD.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnVariantD.Location = new System.Drawing.Point(290, 85);
+            this.btnVariantD.Name = "btnVariantD";
+            this.btnVariantD.Size = new System.Drawing.Size(195, 77);
+            this.btnVariantD.TabIndex = 3;
+            this.btnVariantD.Text = "button4";
+            this.btnVariantD.UseVisualStyleBackColor = true;
+            // 
+            // cbVariantA
+            // 
+            this.cbVariantA.FormattingEnabled = true;
+            this.cbVariantA.Location = new System.Drawing.Point(204, 30);
+            this.cbVariantA.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.cbVariantA.Name = "cbVariantA";
+            this.cbVariantA.Size = new System.Drawing.Size(80, 21);
+            this.cbVariantA.TabIndex = 4;
+            // 
+            // cbVariantC
+            // 
+            this.cbVariantC.FormattingEnabled = true;
+            this.cbVariantC.Location = new System.Drawing.Point(204, 112);
+            this.cbVariantC.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.cbVariantC.Name = "cbVariantC";
+            this.cbVariantC.Size = new System.Drawing.Size(80, 21);
+            this.cbVariantC.TabIndex = 5;
+            // 
+            // cbVariantB
+            // 
+            this.cbVariantB.FormattingEnabled = true;
+            this.cbVariantB.Location = new System.Drawing.Point(491, 30);
+            this.cbVariantB.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.cbVariantB.Name = "cbVariantB";
+            this.cbVariantB.Size = new System.Drawing.Size(83, 21);
+            this.cbVariantB.TabIndex = 6;
+            // 
+            // cbVariantD
+            // 
+            this.cbVariantD.FormattingEnabled = true;
+            this.cbVariantD.Location = new System.Drawing.Point(491, 112);
+            this.cbVariantD.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.cbVariantD.Name = "cbVariantD";
+            this.cbVariantD.Size = new System.Drawing.Size(83, 21);
+            this.cbVariantD.TabIndex = 7;
+            // 
+            // btnApply
+            // 
+            this.btnApply.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnApply.Location = new System.Drawing.Point(711, 319);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(120, 165);
+            this.btnApply.TabIndex = 2;
+            this.btnApply.Text = "Подтвердить";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // MultipleFlags
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 487);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MultipleFlags";
+            this.Text = "MultipleFlags";
+            this.Load += new System.EventHandler(this.MultipleFlags_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFlag1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFlag2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFlag3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFlag4)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.PictureBox pbFlag1;
+        private System.Windows.Forms.PictureBox pbFlag2;
+        private System.Windows.Forms.PictureBox pbFlag3;
+        private System.Windows.Forms.PictureBox pbFlag4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button btnVariantA;
+        private System.Windows.Forms.Button btnVariantB;
+        private System.Windows.Forms.Button btnVariantC;
+        private System.Windows.Forms.Button btnVariantD;
+        private System.Windows.Forms.ComboBox cbVariantA;
+        private System.Windows.Forms.ComboBox cbVariantC;
+        private System.Windows.Forms.ComboBox cbVariantB;
+        private System.Windows.Forms.ComboBox cbVariantD;
+        private System.Windows.Forms.Button btnApply;
+    }
+}
---
+++ FlagsGame/MultipleFlags.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using FlagGame;
+
+namespace FlagsGame
+{
+    public partial class MultipleFlags : Form
+    {
+        string path = @"E:\Development\Kurs3\FlagsGame\FlagGame";
+        Flag flag = new Flag();
+        FlagDatabase flagDb = new FlagDatabase();
+
+        KeyValuePair<int, Flag> kwpFlags = new KeyValuePair<int, Flag>();
+        List<Flag> arrayFlag = new List<Flag>();
+        List<Button> arrayBut = new List<Button>();
+        List<PictureBox> arrayPb = new List<PictureBox>();
+
+        public MultipleFlags()
+        {
+            InitializeComponent();
+        }
+
+        private void MultipleFlags_Load(object sender, EventArgs e)
+        {
+            string pathStats = path + @"\stats.txt";
+            flagDb.LoadXml(path + @"\FlagDatabaseRU.xml");
+            //if (File.ReadAllText(pathStats).Length == 0)
+            //    lbBest.Text = "0";
+            //else
+            //    lbBest.Text = File.ReadAllText(pathStats);
+            loadFlagForm();
+        }
+
+        private void Draw(PictureBox pbFlag)
+        {
+            Random rnd = new Random();
+            FlagPainter flagP = new FlagPainter();
+            Bitmap flagBm = new Bitmap(pbFlag.Width, pbFlag.Height);
+            flag = arrayFlag[rnd.Next(0, arrayFlag.Count)];
+            using (Graphics g = Graphics.FromImage((Image)flagBm))
+                g.DrawImage(flagP.Draw(flag), 0, 0, flagBm.Width, flagBm.Height);
+            arrayFlag.RemoveAt(arrayFlag.IndexOf(flag));
+            pbFlag.Image = flagBm;
+        }
+
+        private void loadFlagForm()
+        {
+            arrayBut.Add(btnVariantA);
+            arrayBut.Add(btnVariantB);
+            arrayBut.Add(btnVariantC);
+            arrayBut.Add(btnVariantD);
+            ShuffleList.Shuffle(arrayBut);
+           
+            arrayPb.Add(pbFlag1);
+            arrayPb.Add(pbFlag2);
+            arrayPb.Add(pbFlag3);
+            arrayPb.Add(pbFlag4);
+
+            foreach (var btn in arrayBut)
+            {
+                btn.Text = flagDb.getRandomFlag().Name;
+
+            }
+
+            arrayFlag = flagDb.workingFlagsList();
+
+            foreach (var pb in arrayPb)
+                Draw(pb);
+
+            arrayFlag = flagDb.workingFlagsList();
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+       //     if(cbVariantA.SelectedIndex == )
+        }
+    }
+}
