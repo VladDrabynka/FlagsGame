e7331aa99a67a0ef1ac6604183053b2a92b077e2
Vlad Drabynka <vlad.un1que.96@gmail.com>
2015-12-12 21:23:08 +0200

    Change the way of saving statistics, fixed some bugs in new gamemode

--- FlagsGame/MultipleFlags.Designer.cs
+++ FlagsGame/MultipleFlags.Designer.cs
@@ -46,10 +46,10 @@
             this.pbFlag4 = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
-            this.lbCountText = new System.Windows.Forms.Label();
-            this.lbCount = new System.Windows.Forms.Label();
-            this.lbBestText = new System.Windows.Forms.Label();
             this.lbBest = new System.Windows.Forms.Label();
+            this.lbBestText = new System.Windows.Forms.Label();
+            this.lbCount = new System.Windows.Forms.Label();
+            this.lbCountText = new System.Windows.Forms.Label();
             this.tableLayoutPanel3.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbFlag1)).BeginInit();
@@ -63,9 +63,9 @@
             // btnApply
             // 
             this.btnApply.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnApply.Location = new System.Drawing.Point(711, 319);
+            this.btnApply.Location = new System.Drawing.Point(819, 342);
             this.btnApply.Name = "btnApply";
-            this.btnApply.Size = new System.Drawing.Size(120, 165);
+            this.btnApply.Size = new System.Drawing.Size(139, 176);
             this.btnApply.TabIndex = 2;
             this.btnApply.Text = "Подтвердить";
             this.btnApply.UseVisualStyleBackColor = true;
@@ -85,20 +85,21 @@
             this.tableLayoutPanel3.Controls.Add(this.cbVariantA, 1, 0);
             this.tableLayoutPanel3.Controls.Add(this.cbVariantB, 1, 1);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(128, 319);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(147, 342);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 4;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(577, 165);
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(666, 176);
             this.tableLayoutPanel3.TabIndex = 1;
             // 
             // cbVariantD
             // 
+            this.cbVariantD.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbVariantD.FormattingEnabled = true;
-            this.cbVariantD.Location = new System.Drawing.Point(338, 133);
+            this.cbVariantD.Location = new System.Drawing.Point(383, 142);
             this.cbVariantD.Margin = new System.Windows.Forms.Padding(50, 10, 3, 3);
             this.cbVariantD.Name = "cbVariantD";
             this.cbVariantD.Size = new System.Drawing.Size(186, 21);
@@ -106,8 +107,9 @@
             // 
             // cbVariantC
             // 
+            this.cbVariantC.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbVariantC.FormattingEnabled = true;
-            this.cbVariantC.Location = new System.Drawing.Point(338, 92);
+            this.cbVariantC.Location = new System.Drawing.Point(383, 98);
             this.cbVariantC.Margin = new System.Windows.Forms.Padding(50, 10, 3, 3);
             this.cbVariantC.Name = "cbVariantC";
             this.cbVariantC.Size = new System.Drawing.Size(186, 21);
@@ -116,8 +118,8 @@
             // lbVariantA
             // 
             this.lbVariantA.AutoSize = true;
-            this.lbVariantA.Location = new System.Drawing.Point(200, 15);
-            this.lbVariantA.Margin = new System.Windows.Forms.Padding(200, 15, 3, 0);
+            this.lbVariantA.Location = new System.Drawing.Point(220, 15);
+            this.lbVariantA.Margin = new System.Windows.Forms.Padding(220, 15, 3, 0);
             this.lbVariantA.Name = "lbVariantA";
             this.lbVariantA.Size = new System.Drawing.Size(41, 13);
             this.lbVariantA.TabIndex = 5;
@@ -126,8 +128,8 @@
             // lbVariantB
             // 
             this.lbVariantB.AutoSize = true;
-            this.lbVariantB.Location = new System.Drawing.Point(200, 56);
-            this.lbVariantB.Margin = new System.Windows.Forms.Padding(200, 15, 3, 0);
+            this.lbVariantB.Location = new System.Drawing.Point(220, 59);
+            this.lbVariantB.Margin = new System.Windows.Forms.Padding(220, 15, 3, 0);
             this.lbVariantB.Name = "lbVariantB";
             this.lbVariantB.Size = new System.Drawing.Size(41, 13);
             this.lbVariantB.TabIndex = 6;
@@ -136,8 +138,8 @@
             // lbVariantC
             // 
             this.lbVariantC.AutoSize = true;
-            this.lbVariantC.Location = new System.Drawing.Point(200, 97);
-            this.lbVariantC.Margin = new System.Windows.Forms.Padding(200, 15, 3, 0);
+            this.lbVariantC.Location = new System.Drawing.Point(220, 103);
+            this.lbVariantC.Margin = new System.Windows.Forms.Padding(220, 15, 3, 0);
             this.lbVariantC.Name = "lbVariantC";
             this.lbVariantC.Size = new System.Drawing.Size(41, 13);
             this.lbVariantC.TabIndex = 7;
@@ -146,8 +148,8 @@
             // lbVariantD
             // 
             this.lbVariantD.AutoSize = true;
-            this.lbVariantD.Location = new System.Drawing.Point(200, 138);
-            this.lbVariantD.Margin = new System.Windows.Forms.Padding(200, 15, 3, 0);
+            this.lbVariantD.Location = new System.Drawing.Point(220, 147);
+            this.lbVariantD.Margin = new System.Windows.Forms.Padding(220, 15, 3, 0);
             this.lbVariantD.Name = "lbVariantD";
             this.lbVariantD.Size = new System.Drawing.Size(41, 13);
             this.lbVariantD.TabIndex = 8;
@@ -155,8 +157,9 @@
             // 
             // cbVariantA
             // 
+            this.cbVariantA.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbVariantA.FormattingEnabled = true;
-            this.cbVariantA.Location = new System.Drawing.Point(338, 10);
+            this.cbVariantA.Location = new System.Drawing.Point(383, 10);
             this.cbVariantA.Margin = new System.Windows.Forms.Padding(50, 10, 3, 3);
             this.cbVariantA.Name = "cbVariantA";
             this.cbVariantA.Size = new System.Drawing.Size(186, 21);
@@ -164,8 +167,9 @@
             // 
             // cbVariantB
             // 
+            this.cbVariantB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbVariantB.FormattingEnabled = true;
-            this.cbVariantB.Location = new System.Drawing.Point(338, 51);
+            this.cbVariantB.Location = new System.Drawing.Point(383, 54);
             this.cbVariantB.Margin = new System.Windows.Forms.Padding(50, 10, 3, 3);
             this.cbVariantB.Name = "cbVariantB";
             this.cbVariantB.Size = new System.Drawing.Size(186, 21);
@@ -181,12 +185,12 @@
             this.tableLayoutPanel2.Controls.Add(this.pbFlag3, 0, 1);
             this.tableLayoutPanel2.Controls.Add(this.pbFlag4, 1, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(128, 3);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(147, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 2;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(577, 310);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(666, 333);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
             // pbFlag1
@@ -194,34 +198,34 @@
             this.pbFlag1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pbFlag1.Location = new System.Drawing.Point(3, 3);
             this.pbFlag1.Name = "pbFlag1";
-            this.pbFlag1.Size = new System.Drawing.Size(282, 149);
+            this.pbFlag1.Size = new System.Drawing.Size(327, 160);
             this.pbFlag1.TabIndex = 0;
             this.pbFlag1.TabStop = false;
             // 
             // pbFlag2
             // 
             this.pbFlag2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pbFlag2.Location = new System.Drawing.Point(291, 3);
+            this.pbFlag2.Location = new System.Drawing.Point(336, 3);
             this.pbFlag2.Name = "pbFlag2";
-            this.pbFlag2.Size = new System.Drawing.Size(283, 149);
+            this.pbFlag2.Size = new System.Drawing.Size(327, 160);
             this.pbFlag2.TabIndex = 1;
             this.pbFlag2.TabStop = false;
             // 
             // pbFlag3
             // 
             this.pbFlag3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pbFlag3.Location = new System.Drawing.Point(3, 158);
+            this.pbFlag3.Location = new System.Drawing.Point(3, 169);
             this.pbFlag3.Name = "pbFlag3";
-            this.pbFlag3.Size = new System.Drawing.Size(282, 149);
+            this.pbFlag3.Size = new System.Drawing.Size(327, 161);
             this.pbFlag3.TabIndex = 2;
             this.pbFlag3.TabStop = false;
             // 
             // pbFlag4
             // 
             this.pbFlag4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pbFlag4.Location = new System.Drawing.Point(291, 158);
+            this.pbFlag4.Location = new System.Drawing.Point(336, 169);
             this.pbFlag4.Name = "pbFlag4";
-            this.pbFlag4.Size = new System.Drawing.Size(283, 149);
+            this.pbFlag4.Size = new System.Drawing.Size(327, 161);
             this.pbFlag4.TabIndex = 3;
             this.pbFlag4.TabStop = false;
             // 
@@ -241,25 +245,56 @@
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 65.0924F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34.9076F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(834, 487);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(961, 521);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // tableLayoutPanel4
             // 
             this.tableLayoutPanel4.ColumnCount = 2;
-            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 79.83193F));
-            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.16807F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 78.9855F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 21.01449F));
             this.tableLayoutPanel4.Controls.Add(this.lbBest, 1, 1);
             this.tableLayoutPanel4.Controls.Add(this.lbBestText, 0, 1);
             this.tableLayoutPanel4.Controls.Add(this.lbCount, 1, 0);
             this.tableLayoutPanel4.Controls.Add(this.lbCountText, 0, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel4.Name = "tableLayoutPanel4";
             this.tableLayoutPanel4.RowCount = 2;
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.6129F));
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.3871F));
-            this.tableLayoutPanel4.Size = new System.Drawing.Size(119, 310);
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(138, 333);
             this.tableLayoutPanel4.TabIndex = 3;
+            // 
+            // lbBest
+            // 
+            this.lbBest.AutoSize = true;
+            this.lbBest.Location = new System.Drawing.Point(111, 53);
+            this.lbBest.Margin = new System.Windows.Forms.Padding(3, 15, 0, 0);
+            this.lbBest.Name = "lbBest";
+            this.lbBest.Size = new System.Drawing.Size(13, 13);
+            this.lbBest.TabIndex = 4;
+            this.lbBest.Text = "0";
+            // 
+            // lbBestText
+            // 
+            this.lbBestText.AutoSize = true;
+            this.lbBestText.Location = new System.Drawing.Point(15, 53);
+            this.lbBestText.Margin = new System.Windows.Forms.Padding(15, 15, 0, 0);
+            this.lbBestText.Name = "lbBestText";
+            this.lbBestText.Size = new System.Drawing.Size(63, 13);
+            this.lbBestText.TabIndex = 3;
+            this.lbBestText.Text = "Best streak:";
+            // 
+            // lbCount
+            // 
+            this.lbCount.AutoSize = true;
+            this.lbCount.Location = new System.Drawing.Point(111, 15);
+            this.lbCount.Margin = new System.Windows.Forms.Padding(3, 15, 0, 0);
+            this.lbCount.Name = "lbCount";
+            this.lbCount.Size = new System.Drawing.Size(13, 13);
+            this.lbCount.TabIndex = 2;
+            this.lbCount.Text = "0";
             // 
             // lbCountText
             // 
@@ -271,45 +306,16 @@
             this.lbCountText.TabIndex = 1;
             this.lbCountText.Text = "Right answers:";
             // 
-            // lbCount
-            // 
-            this.lbCount.AutoSize = true;
-            this.lbCount.Location = new System.Drawing.Point(97, 15);
-            this.lbCount.Margin = new System.Windows.Forms.Padding(3, 15, 0, 0);
-            this.lbCount.Name = "lbCount";
-            this.lbCount.Size = new System.Drawing.Size(13, 13);
-            this.lbCount.TabIndex = 2;
-            this.lbCount.Text = "0";
-            // 
-            // lbBestText
-            // 
-            this.lbBestText.AutoSize = true;
-            this.lbBestText.Location = new System.Drawing.Point(15, 51);
-            this.lbBestText.Margin = new System.Windows.Forms.Padding(15, 15, 0, 0);
-            this.lbBestText.Name = "lbBestText";
-            this.lbBestText.Size = new System.Drawing.Size(63, 13);
-            this.lbBestText.TabIndex = 3;
-            this.lbBestText.Text = "Best streak:";
-            // 
-            // lbBest
-            // 
-            this.lbBest.AutoSize = true;
-            this.lbBest.Location = new System.Drawing.Point(97, 51);
-            this.lbBest.Margin = new System.Windows.Forms.Padding(3, 15, 0, 0);
-            this.lbBest.Name = "lbBest";
-            this.lbBest.Size = new System.Drawing.Size(13, 13);
-            this.lbBest.TabIndex = 4;
-            this.lbBest.Text = "0";
-            // 
             // MultipleFlags
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(834, 487);
+            this.ClientSize = new System.Drawing.Size(961, 521);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MultipleFlags";
-            this.Text = "MultipleFlags";
+            this.Text = "Flags";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MultipleFlags_FormClosing);
             this.Load += new System.EventHandler(this.MultipleFlags_Load);
             this.SizeChanged += new System.EventHandler(this.MultipleFlags_SizeChanged);
             this.tableLayoutPanel3.ResumeLayout(false);
